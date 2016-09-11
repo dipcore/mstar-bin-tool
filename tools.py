40b52eae258886a38bceee9024dd369ddd169039
dipcore <dipcore@gmail.com>
2016-09-10 22:53:27 -0400

    init

---
+++ tools.py
@@ -0,0 +1,147 @@
+import math
+import os
+import re
+import sys
+
+KB = 2**10
+MB = 2**20
+GB = 2**30
+
+def size(s):
+	 if (s == 0):
+			 return '0B'
+	 size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+	 i = int(math.floor(math.log(s, 1024)))
+	 p = math.pow(1024 ,i)
+	 s = round(s / p, 2)
+	 return '%s %s' % (s,size_name[i])
+
+# Append src file to dest file
+# bufsize - chunk size
+def appenFile(src, dest, bufsize = 16 * MB):
+	with open(src, 'rb') as f1:
+		with open(dest, 'ab') as f2:
+			data = True
+			while data:
+				data = f1.read(bufsize)
+				f2.write(data)
+	
+# Copy part of src file to dest file. 
+# offset - beginning of the part to copy
+# size - length of the part to copy
+# bufsize - chunk size
+# append - if True then append to dest file
+# if dest file does not exist it will be created
+def copyPart(src, dest, offset, size, bufsize = 16 * MB, append = False):
+	if not os.path.exists(dest):
+		append = False
+
+	with open(src, 'rb') as f1:
+		f1.seek(offset)
+		with open(dest, 'ab' if append else 'wb') as f2:
+			while size:
+				chunk = min(bufsize, size)
+				data = f1.read(chunk)
+				f2.write(data)
+				size -= chunk
+
+# Trim part of src file and save the rest to dest file.
+# offset - beginning of the part to trim
+# size - length of the part to trim
+# bufsize - chunk size
+def trimPart(src, dest, offset, size, bufsize = 16 * MB):
+	srcSize = os.path.getsize(src)
+	copyPart(src, dest, 0, offset, bufsize)
+	copyPart(src, dest, offset + size, srcSize - offset - size, bufsize = bufsize, append = True)
+
+# Load and return part
+# file - source file
+# offset - beginning of the part
+# size - length of the part
+def loadPart(file, offset, size):
+	with open(file, 'rb') as f:
+		f.seek(offset)
+		return f.read(size)
+
+def parceArgs(string):
+	return re.findall('([^\s]+)', string)
+
+def processFilePartLoad(line):
+	args = parceArgs(line)
+	return {"cmd": args[0], "addr": args[1], "sourceFile": args[2], "offset": args[3], "size": args[4]}
+	
+def processStoreSecureInfo(line):
+	args = parceArgs(line)
+	return {"cmd": args[0], "partition_name": args[1], "addr": args[2]}	
+	
+def processStoreNuttxConfig(line):
+	args = parceArgs(line)
+	return {"cmd": args[0], "partition_name": args[1], "addr": args[2]}
+
+def processMmc(line):
+	args = parceArgs(line)
+
+	if args[1] == "erase.p":
+		# mmc erase.p partition_name
+		return {"cmd": args[0], "action": args[1], "partition_name": args[2]}
+
+	elif args[1] == "write.p":
+		# mmc write.p addr partition_name size [empty_skip:0-disable,1-enable]
+		res = {"cmd": args[0], "action": args[1], "addr": args[2], "partition_name": args[3], "size": args[4]}
+		try:
+			res["empty_skip"] = args[5]
+		except IndexError:
+			res["empty_skip"] = 0
+		return res
+
+	elif args[1] == "write.p.continue" or args[1] == "write.p.cont":
+		# mmc write.p(.continue|.cont) addr partition_name offset size [empty_skip:0-disable,1-enable]\n
+		res = {"cmd": args[0], "action": "write.p.continue", "addr": args[2], "partition_name": args[3], "offset": args[4], "size": args[5]}
+		try:
+			res["empty_skip"] = args[6]
+		except IndexError:
+			res["empty_skip"] = 0
+		return res
+
+
+	elif args[1] == "write.boot" or args[1] == "write":
+		# mmc write[.boot] bootpart addr blk# size [empty_skip:0-disable,1-enable]
+		res = {"cmd": args[0], "action": args[1], "bootpart": args[2], "addr": args[3], "blk#": args[4], "size": args[5], "partition_name": "sboot"}
+		try:
+			res["empty_skip"] = args[6]
+		except IndexError:
+			res["empty_skip"] = 0
+		return res
+
+	elif args[1] == "unlzo":
+		# mmc unlzo[.continue|.cont] addr size partition_name [empty_skip:0-disable,1-enable]- decompress lzo file and write to mmc partition
+		res = {"cmd": args[0], "action": args[1], "addr": args[2], "size": args[3], "partition_name": args[4]}
+		try:
+			res["empty_skip"] = args[5]
+		except IndexError:
+			res["empty_skip"] = 0
+		return res
+
+	elif args[1] == "unlzo.continue" or args[1] == "unlzo.cont":
+		# mmc unlzo[.continue|.cont] addr size partition_name [empty_skip:0-disable,1-enable]- decompress lzo file and write to mmc partition
+		res = {"cmd": args[0], "action": "unlzo.continue", "addr": args[2], "size": args[3], "partition_name": args[4]}
+		try:
+			res["empty_skip"] = args[5]
+		except IndexError:
+			res["empty_skip"] = 0
+		return res
+
+	# else:
+	# 	print "Unknown mmc action"
+	# 	print args
+
+fileNameCounter = {}
+def generateFileName(outputDirectory, part, ext):
+	fileName = os.path.join(outputDirectory, part["partition_name"] + ext)
+	if os.path.exists(fileName):
+		try:
+			fileNameCounter[part["partition_name"]] += 1
+		except:
+			fileNameCounter[part["partition_name"]] = 1
+		fileName = os.path.join(outputDirectory, part["partition_name"] + str(fileNameCounter[part["partition_name"]]) + ext)
+	return fileName
---
+++ unpack.py
@@ -0,0 +1,144 @@
+import sys
+import os
+import re
+import shutil
+from subprocess import call
+
+import tools
+
+DEBUG = False
+HEADER_SIZE = 16 * tools.KB # Header size is always 16KB
+#DEBUG = True
+
+# Vars
+headerScript = ""
+headerScriptFound = False
+counter = {};
+
+# Parse args
+if len(sys.argv) == 1: 
+	print "Usage: unpack.py <MStar bin firmware> <Path to store unpacked parts [default: ./unpacked/]>"
+	quit()
+
+inputFile = sys.argv[1]
+if not os.path.exists(inputFile):
+	print "No such file: %s" % inputFile
+	quit()
+
+if 2 in sys.argv:
+	outputDirectory = sys.argv[2]
+else:
+	outputDirectory = 'unpacked'
+
+# Create output directory
+if os.path.exists(outputDirectory):
+	shutil.rmtree(outputDirectory)
+os.makedirs(outputDirectory)
+
+# Find header script
+# Header max size is 16KB
+# Non used part is filled by 0xFF
+print "[i] Analizing header ..."
+header = tools.loadPart(inputFile, 0, HEADER_SIZE)
+offset = header.find('\xff')
+if offset != -1:
+	headerScript = header[:offset]
+	headerScriptFound = True
+
+if not headerScriptFound:
+	print "[!] Could not find header script!"
+	quit()
+	
+if DEBUG:
+	print headerScript
+
+# Save the script
+print "[i] Saving header script to " + os.path.join(outputDirectory, ".header_script") + " ..."
+with open(os.path.join(outputDirectory, ".header_script"), "w") as f:
+	f.write(headerScript)
+	f.close()
+
+# Parse script
+print "[i] Parsing script ..."
+# Supporting filepartload, mmc, store_secure_info, store_nuttx_config
+for line in headerScript.splitlines():
+
+	if DEBUG:
+		print line
+
+	if re.match("^filepartload", line):
+		params = tools.processFilePartLoad(line)
+		offset =  params["offset"]
+		size =  params["size"]
+
+	if re.match("^store_secure_info", line):		
+	 	params = tools.processStoreSecureInfo(line)
+	 	outputFile = os.path.join(outputDirectory, params["partition_name"])
+	 	tools.copyPart(inputFile, outputFile, int(offset, 16), int(size, 16))
+
+	if re.match("^store_nuttx_config", line):
+	 	params = tools.processStoreNuttxConfig(line)
+	 	outputFile = os.path.join(outputDirectory, params["partition_name"])
+	 	tools.copyPart(inputFile, outputFile, int(offset, 16), int(size, 16))
+
+	if re.match("^mmc", line):
+		params = tools.processMmc(line)
+
+		if params:
+
+			if params["action"] == "write.boot":
+				outputFile = tools.generateFileName(outputDirectory, params, ".img")
+				tools.copyPart(inputFile, outputFile, int(offset, 16), int(size, 16))
+				print "[i] Partition: %s\tOffset: %s\tSize %s (%s) -> %s" % (params["partition_name"], offset, size, tools.size(int(size, 16)), outputFile)
+
+			if params["action"] == "write.p":
+				outputFile = os.path.join(outputDirectory, params["partition_name"] + ".img")
+				tools.copyPart(inputFile, outputFile, int(offset, 16), int(size, 16))
+				print "[i] Partition: %s\tOffset: %s\tSize %s (%s) -> %s" % (params["partition_name"], offset, size, tools.size(int(size, 16)), outputFile)
+
+			if params["action"] == "write.p.continue":
+				outputFile = os.path.join(outputDirectory, params["partition_name"] + ".img")				
+				tools.copyPart(inputFile, outputFile, int(offset, 16), int(size, 16), append = True)
+				print "[i] Partition: %s\tOffset: %s\tSize %s (%s) append to %s" % (params["partition_name"], offset, size, tools.size(int(size, 16)), outputFile)
+
+			if params["action"] == "unlzo":
+				outputLzoFile = tools.generateFileName(outputDirectory, params, ".lzo")
+				outputImgFile = tools.generateFileName(outputDirectory, params, ".img")
+				# save .lzo
+				print "[i] Partition: %s\tOffset: %s\tSize %s (%s) -> %s" % (params["partition_name"], offset, size, tools.size(int(size, 16)), outputLzoFile)
+				tools.copyPart(inputFile, outputLzoFile, int(offset, 16), int(size, 16))
+				# unpack .lzo -> .img
+				print "[i]     Unpacking LZO (Please be patient) %s -> %s" % (outputLzoFile, outputImgFile)
+				call(["./bin/lzop", "-o",  outputImgFile, "-x", outputLzoFile])
+				# delete .lzo
+				os.remove(outputLzoFile)
+
+
+
+			if params["action"] == "unlzo.continue":
+				if not params["partition_name"] in counter:
+					counter[params["partition_name"]] = 0
+				counter[params["partition_name"]] += 1
+
+				outputFile = os.path.join(outputDirectory, params["partition_name"] + ".img")
+				outputChunkLzoFile = os.path.join(outputDirectory, params["partition_name"] + str(counter[params["partition_name"]]) + ".lzo")
+				outputChunkImgFile = os.path.join(outputDirectory, params["partition_name"] + str(counter[params["partition_name"]]) + ".img")
+				# save .lzo
+				print "[i] Partition: %s\tOffset: %s\tSize %s (%s) -> %s" % (params["partition_name"], offset, size, tools.size(int(size, 16)), outputChunkLzoFile)
+				tools.copyPart(inputFile, outputChunkLzoFile, int(offset, 16), int(size, 16))
+				# unpack chunk .lzo -> .img
+				print "[i]     Unpacking LZO (Please be patient) %s -> %s" % (outputChunkLzoFile, outputChunkImgFile)
+				call(["./bin/lzop", "-o",  outputChunkImgFile, "-x", outputChunkLzoFile])
+				# append the chunk to main .img
+				print "[i]     %s append to %s" % (outputChunkImgFile, outputImgFile)
+				tools.appenFile(outputChunkImgFile, outputImgFile)
+				# delete chunk
+				os.remove(outputChunkLzoFile)
+				os.remove(outputChunkImgFile)
+				
+
+
+
+
+
+
